c9dac93853888434bcc67bc82f6081e7bdf0cbed
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-03-19 13:48:42 +0530

    Create Kadane’s Algorithm : Maximum Subarray Sum in an Array.cpp

---
+++ Arrays/Kadane’s Algorithm : Maximum Subarray Sum in an Array.cpp
@@ -0,0 +1,45 @@
+https://takeuforward.org/data-structure/kadanes-algorithm-maximum-subarray-sum-in-an-array/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int maxSubArray(vector < int > & nums, vector < int > & subarray) {
+  int max_sum = INT_MIN;
+  for (int i = 0; i < nums.size(); i++) {
+    int curr_sum = 0;
+    for (int j = i; j < nums.size(); j++) {
+      curr_sum += nums[j];
+      if (curr_sum > max_sum) {
+        subarray.clear();
+        max_sum = curr_sum;
+        subarray.push_back(i);
+        subarray.push_back(j);
+      }
+    }
+  }
+  return max_sum;
+}
+
+int main() {
+  vector<int> arr{-2,1,-3,4,-1,2,1,-5,4};
+  vector < int > subarray;
+  int lon = maxSubArray(arr, subarray);
+  cout << "The longest subarray with maximum sum is " << lon << endl;
+  cout << "The subarray is " << endl;
+  for (int i = subarray[0]; i <= subarray[1]; i++) {
+    cout << arr[i] << " ";
+  }
+
+}
+/*
+Output:
+
+The longest subarray with maximum sum is 6
+The subarray is
+4 -1 2 1
+
+Time Complexity: O(N^2) 
+
+Space Complexity: O(1)
+
+*/
