3894d0d12a2690dc51a29e2a6202786901bf8377
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-03-19 15:09:34 +0530

    Create Stock Buy And Sell.cpp

---
+++ Arrays/Stock Buy And Sell.cpp
@@ -0,0 +1,30 @@
+https://takeuforward.org/data-structure/stock-buy-and-sell/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
+/*
+Output: Max profit is: 5
+
+Time complexity: O(n)
+
+Space Complexity: O(1)
+*/
