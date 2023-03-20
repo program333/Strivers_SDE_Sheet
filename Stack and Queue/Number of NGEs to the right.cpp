73733e724a1143f3c24c210f9a18a004238c71ab
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-03-21 00:10:36 +0530

    Create Number of NGEs to the right.cpp

---
+++ Stack and Queue/Number of NGEs to the right.cpp
@@ -0,0 +1,42 @@
+
+https://www.geeksforgeeks.org/number-nges-right/
+
+// C++ code for the above approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find number of next
+// greater elements on the right of
+// a given element
+int nextGreaterElements(vector<int>& a, int index)
+{
+	int count = 0, N = a.size();
+	for (int i = index + 1; i < N; i++)
+		if (a[i] > a[index])
+			count++;
+
+	return count;
+}
+
+// Driver's code
+int main()
+{
+
+	vector<int> a = { 3, 4, 2, 7, 5, 8, 10, 6 };
+	int Q = 2;
+	vector<int> queries = { 0, 5 };
+
+	for (int i = 0; i < Q; i++)
+		// Function call
+		cout << nextGreaterElements(a, queries[i]) << " ";
+
+	return 0;
+}
+
+/*
+Output
+6 1 
+Time Complexity: O(NQ), and O(N) to answer a single query
+Auxiliary space: O(1) 
+*/
