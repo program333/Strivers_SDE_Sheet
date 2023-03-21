0f9553332648bc7af300db6e68b66ec039b613de
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-03-21 22:40:59 +0530

    Update Detect loop in a linked list.cpp

--- LinkedList/Detect loop in a linked list.cpp
+++ LinkedList/Detect loop in a linked list.cpp
@@ -76,3 +76,73 @@ Loop Found
 Time complexity: O(N), Only one traversal of the loop is needed.
 Auxiliary Space: O(N), N is the space required to store the value in the hashmap.
 */
+
+
+//Detect loop in a linked list using Floyd’s Cycle-Finding Algorithm:
+
+// C++ program to detect loop in a linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+class Node {
+public:
+	int data;
+	Node* next;
+};
+
+void push(Node** head_ref, int new_data)
+{
+	/* allocate node */
+	Node* new_node = new Node();
+
+	/* put in the data */
+	new_node->data = new_data;
+
+	/* link the old list of the new node */
+	new_node->next = (*head_ref);
+
+	/* move the head to point to the new node */
+	(*head_ref) = new_node;
+}
+
+int detectLoop(Node* list)
+{
+	Node *slow_p = list, *fast_p = list;
+
+	while (slow_p && fast_p && fast_p->next) {
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/* Driver code*/
+int main()
+{
+	/* Start with the empty list */
+	Node* head = NULL;
+
+	push(&head, 20);
+	push(&head, 4);
+	push(&head, 15);
+	push(&head, 10);
+
+	/* Create a loop for testing */
+	head->next->next->next->next = head;
+	if (detectLoop(head))
+		cout << "Loop Found";
+	else
+		cout << "No Loop";
+	return 0;
+}
+
+/*
+Output
+Loop Found
+Time complexity: O(N), Only one traversal of the loop is needed.
+Auxiliary Space: O(1). 
+*/
