b2b15709b759da9eec65713a17bbb113e3b4a4a2
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-03-22 20:19:05 +0530

    Create Remove Linked List Elements.cpp

---
+++ LinkedList/Remove Linked List Elements.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/remove-linked-list-elements/description/
+
+/*
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+*/
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==nullptr) return head;
+		// This is for the case when a linked list is like this: 
+		// 1->1->2->null , val = 1
+		// 1->1->1->null , val = 1
+        while(head!=nullptr && head->val==val){
+            head = head->next;
+        }
+        ListNode* curr = head;
+        while(curr!=nullptr  && curr->next!=nullptr){
+            if(curr->next->val==val){
+                curr->next = curr->next->next;
+				// After doing the above step, I am not updating "curr" because of these type of test cases:
+				// 1->2->3->6->6->6->5->null  val = 6
+            }
+            else
+                curr = curr->next;
+        }
+        return head;
+    }
+};
