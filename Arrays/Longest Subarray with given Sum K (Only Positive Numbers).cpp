0d62ae291da98ecbdcd42142996918d94cec2287
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-04 01:43:30 +0530

    Update Longest Subarray with given Sum K (Only Positive Numbers).cpp

--- Arrays/Longest Subarray with given Sum K (Only Positive Numbers).cpp
+++ Arrays/Longest Subarray with given Sum K (Only Positive Numbers).cpp
@@ -47,3 +47,43 @@ Time Complexity: O(n^2) time to generate all possible subarrays.
 
 Space Complexity: O(1), we are not using any extra space
 */
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int longestSubArrWithSumK_Optimal(int arr[], int n, int k) {
+  int start = 0, end = -1, sum = 0, maxLength = 0;
+  while (start < n) {
+    while ((end + 1 < n) && (sum + arr[end + 1] <= k))
+      sum += arr[++end];
+
+    if (sum == k)
+      maxLength = max(maxLength, (end - start + 1));
+
+    sum -= arr[start];
+    start++;
+  }
+  return maxLength;
+}
+
+int main() {
+
+  int arr[] = {7,1,6,0};
+  int n = sizeof(arr) / sizeof(arr[0]), k = 7;
+
+  cout << "Length of the longest subarray with sum K is " << 
+  longestSubArrWithSumK_Optimal(arr, n, k);
+
+  return 0;
+}
+
+/*
+output: Length of the longest subarray with sum K is 3
+
+Time Complexity: O(2n) ~ O(n), if we observe closely, the window only forwards to the right always. 
+And every element is visited at most 2 times, one by the end variable and by the start variable.
+
+Space Complexity: O(1), we are not using any extra space.
+*/
