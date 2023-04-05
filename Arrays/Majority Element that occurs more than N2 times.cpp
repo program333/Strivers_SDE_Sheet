53b54b4f706b1d56ffbf2c55409f25be3425b271
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-05 12:18:44 +0530

    Create Majority Element that occurs more than N2 times.cpp

---
+++ Arrays/Majority Element that occurs more than N2 times.cpp
@@ -0,0 +1,48 @@
+https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+
+    //declaring a map:
+    map<int, int> mpp;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+    }
+
+    //searching for the majority element:
+    for (auto it : mpp) {
+        if (it.second > (n / 2)) {
+            return it.first;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
+    return 0;
+}
+
+/*
+Output: The majority element is: 2
+
+Time Complexity: O(N*logN) + O(N), where N = size of the given array.
+Reason: We are using a map data structure. Insertion in the map takes logN time. And we are doing it for N elements. 
+So, it results in the first term O(N*logN). The second O(N) is for checking which element occurs more than floor(N/2) times.
+If we use unordered_map instead, the first term will be O(N) for the best and average case and for the worst case, it will be O(N2).
+
+Space Complexity: O(N) as we are using a map data structure.
+
+*/
