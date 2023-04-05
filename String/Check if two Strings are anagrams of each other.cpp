942f2b7d3e92be71b8f43d0a59a96a34610781e2
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-05 15:17:13 +0530

    Create Check if two Strings are anagrams of each other.cpp

---
+++ String/Check if two Strings are anagrams of each other.cpp
@@ -0,0 +1,116 @@
+https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other/
+https://www.geeksforgeeks.org/check-whether-two-strings-are-anagram-of-each-other/
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool CheckAnagrams(string str1, string str2)
+{
+  // Case 1: when both of the strings have different lengths
+  if (str1.length() != str2.length())
+    return false;
+ 
+  sort(str1.begin(), str1.end());
+  sort(str2.begin(), str2.end());
+ 
+  // Case 2: check if every character of str1 and str2 matches with each other
+  for (int i = 0; i < str1.length(); i++)
+  {
+    if (str1[i] != str2[i])
+      return false;
+  }
+  return true;
+}
+int main()
+{
+  string Str1 = "INTEGER";
+  string Str2 = "TEGERNI";
+  if(CheckAnagrams(Str1, Str2))
+  cout << "True" << endl;
+  else
+  cout<<"False"<<endl;
+  return 0;
+}
+/*
+Output: True
+
+Time Complexity: O(nlogn) since sorting function requires nlogn iterations.
+
+Space Complexity: O(1)
+*/
+
+// C++ implementation of the approach
+// Function that returns true if a and b
+// are anagarams of each other
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string a, string b)
+{
+
+	// Check if length of both strings is same or not
+	if (a.length() != b.length()) {
+		return false;
+	}
+	// Create a HashMap containing Character as Key and
+	// Integer as Value. We will be storing character as
+	// Key and count of character as Value.
+	unordered_map<char, int> Map;
+	// Loop over all character of String a and put in
+	// HashMap.
+	for (int i = 0; i < a.length(); i++) {
+		Map[a[i]]++;
+	}
+	// Now loop over String b
+	for (int i = 0; i < b.length(); i++) {
+		// Check if current character already exists in
+		// HashMap/map
+		if (Map.find(b[i]) != Map.end()) {
+			// If contains reduce count of that
+			// character by 1 to indicate that current
+			// character has been already counted as
+			// idea here is to check if in last count of
+			// all characters in last is zero which
+			// means all characters in String a are
+			// present in String b.
+			Map[b[i]] -= 1;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// Loop over all keys and check if all keys are 0.
+	// If so it means it is anagram.
+	for (auto items : Map) {
+		if (items.second != 0) {
+			return false;
+		}
+	}
+	// Returning True as all keys are zero
+	return true;
+}
+
+// Driver code
+int main()
+{
+	string str1 = "gram";
+	string str2 = "arm";
+
+	// Function call
+	if (isAnagram(str1, str2))
+		cout << "The two strings are anagram of each other"
+			<< endl;
+	else
+		cout << "The two strings are not anagram of each "
+				"other"
+			<< endl;
+}
+
+/*
+Output
+The two strings are not anagram of each other
+Time Complexity: O(N)
+Auxiliary Space: O(1), constant space is used
+*/
