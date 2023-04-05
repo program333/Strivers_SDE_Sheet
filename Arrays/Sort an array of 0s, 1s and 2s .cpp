87097ecb6c2d66bc6031fd006d45b9e4a27d9c10
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-05 12:17:00 +0530

    Update Sort an array of 0s, 1s and 2s .cpp

--- Arrays/Sort an array of 0s, 1s and 2s .cpp
+++ Arrays/Sort an array of 0s, 1s and 2s .cpp
@@ -1,61 +1,96 @@
 https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/
 
-// C++ program to sort an array
-// with 0, 1 and 2 in a single pass
+https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to sort the input array,
-// the array is assumed
-// to have values in {0, 1, 2}
-void sort012(int a[], int arr_size)
-{
-	int lo = 0;
-	int hi = arr_size - 1;
-	int mid = 0;
+void sortArray(vector<int>& arr, int n) {
 
-	// Iterate till all the elements
-	// are sorted
-	while (mid <= hi) {
-		switch (a[mid]) {
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
 
-		// If the element is 0
-		case 0:
-			swap(a[lo++], a[mid++]);
-			break;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 0) cnt0++;
+        else if (arr[i] == 1) cnt1++;
+        else cnt2++;
+    }
 
-		// If the element is 1 .
-		case 1:
-			mid++;
-			break;
+    //Replace the places in the original array:
+    for (int i = 0; i < cnt0; i++) arr[i] = 0; // replacing 0's
 
-		// If the element is 2
-		case 2:
-			swap(a[mid], a[hi--]);
-			break;
-		}
-	}
+    for (int i = cnt0; i < cnt0 + cnt1; i++) arr[i] = 1; // replacing 1's
+
+    for (int i = cnt0 + cnt1; i < n; i++) arr[i] = 2; // replacing 2's
+
 }
 
-// Function to print array arr[]
-void printArray(int arr[], int arr_size)
-{
-	// Iterate and print every element
-	for (int i = 0; i < arr_size; i++)
-		cout << arr[i] << " ";
-}
-
-// Driver Code
 int main()
 {
-	int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+    int n = 6;
+    vector<int> arr = {0, 2, 1, 2, 0, 1};
+    sortArray(arr, n);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+/*
+Output:
+After sorting:
+0 0 1 1 2 2 
 
-	sort012(arr, n);
+Time Complexity: O(N) + O(N), where N = size of the array. First O(N) for counting the number of 0’s, 1’s, 2’s, and second O(N) for placing them 
+correctly in the original array.
 
-	printArray(arr, n);
+Space Complexity: O(1) as we are not using any extra space.
+*/
 
-	return 0;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(vector<int>& arr, int n) {
+
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
 }
 
+int main()
+{
+    int n = 6;
+    vector<int> arr = {0, 2, 1, 2, 0, 1};
+    sortArray(arr, n);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
 
+/*
+Output:
+After sorting:
+0 0 1 1 2 2  
+
+Time Complexity: O(N), where N = size of the given array.
+Reason: We are using a single loop that can run at most N times.
+*/
+
+Space Complexity: O(1) as we are not using any extra space.
