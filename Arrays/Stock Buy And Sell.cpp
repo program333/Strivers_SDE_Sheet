38f48a477d31d62bba2afb8509fc3695510ecf7f
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-05 11:55:06 +0530

    Update Stock Buy And Sell.cpp

--- Arrays/Stock Buy And Sell.cpp
+++ Arrays/Stock Buy And Sell.cpp
@@ -1,5 +1,16 @@
 https://takeuforward.org/data-structure/stock-buy-and-sell/
 
+/*
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and 
+sell on day 5 (price = 6), profit = 6-1 = 5.
+
+Note: That buying on day 2 and selling on day 1 
+is not allowed because you must buy before 
+you sell.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
