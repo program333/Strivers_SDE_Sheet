6184f407230c2a4bef695e69d1238874f1eb934a
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-06 22:48:59 +0530

    Update Next Smaller Element.cpp

--- Stack and Queue/Next Smaller Element.cpp
+++ Stack and Queue/Next Smaller Element.cpp
@@ -2,6 +2,43 @@
 https://www.geeksforgeeks.org/next-smaller-element/
 
 
+
+// Simple C++ program to print
+// next smaller elements in a given array
+#include "bits/stdc++.h"
+using namespace std;
+
+/* prints element and NSE pair
+for all elements of arr[] of size n */
+void printNSE(int arr[], int n)
+{
+	int next, i, j;
+	for (i = 0; i < n; i++)
+	{
+		next = -1;
+		for (j = i + 1; j < n; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				next = arr[j];
+				break;
+			}
+		}
+		cout << arr[i] << " -- "
+			<< next << endl;
+	}
+}
+
+// Driver Code
+int main()
+{
+	int arr[]= {11, 13, 21, 3};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	printNSE(arr, n);
+	return 0;
+}
+
+
 // A Stack based C++ program to find next
 // smaller element for all array elements
 #include <bits/stdc++.h>
