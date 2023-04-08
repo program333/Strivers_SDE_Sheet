eeb4d5cce9d75a6c3262a6e955df313999a36633
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-09 02:01:18 +0530

    Create Last occurrence in a sorted array.cpp

---
+++ Binary Search/Last occurrence in a sorted array.cpp
@@ -0,0 +1,44 @@
+https://takeuforward.org/data-structure/last-occurrence-in-a-sorted-array/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, int key, vector < int > & v) {
+  int start = 0;
+  int end = n - 1;
+  int res = -1;
+
+  while (start <= end) {
+    int mid = start + (end - start) / 2;
+    if (v[mid] == key) {
+      res = mid;
+      start = mid + 1;
+
+    } else if (key < v[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return res;
+}
+
+int main() {
+  int n = 7;
+  int key = 13;
+  vector < int > v = {3,4,13,13,13,20,40};
+  
+  // returning the last occurrence index if the element is present otherwise -1
+  cout << solve(n, key, v) << "\n";
+
+  return 0;
+}
+
+/*
+Output: 4
+
+Time Complexity: O(log n)
+
+Space Complexity: O(1)
