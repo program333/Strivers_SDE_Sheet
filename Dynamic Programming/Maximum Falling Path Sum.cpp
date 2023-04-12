b4fb6ea1eea9217d332ba704a2150e2a9205e2ce
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-12 15:45:18 +0530

    Create Maximum Falling Path Sum.cpp

---
+++ Dynamic Programming/Maximum Falling Path Sum.cpp
@@ -0,0 +1,63 @@
+https://takeuforward.org/data-structure/minimum-maximum-falling-path-sum-dp-12/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getMaxUtil(int i, int j, int m, vector<vector<int>> &matrix, 
+vector<vector<int> > &dp){
+    
+    // Base Conditions
+    if(j<0 || j>=m)
+        return -1e9;
+    if(i==0)
+        return matrix[0][j];
+    
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    int up = matrix[i][j] + getMaxUtil(i-1,j,m,matrix,dp);
+    int leftDiagonal = matrix[i][j] + getMaxUtil(i-1,j-1,m,matrix,dp);
+    int rightDiagonal = matrix[i][j] + getMaxUtil(i-1,j+1,m,matrix,dp);
+    
+    return dp[i][j]= max(up,max(leftDiagonal,rightDiagonal));
+    
+}
+
+int getMaxPathSum(vector<vector<int> > &matrix){
+    
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    
+    int maxi = INT_MIN;
+    
+    for(int j=0; j<m;j++){
+        int ans = getMaxUtil(n-1,j,m,matrix,dp);
+        maxi = max(maxi,ans);
+    }
+    
+    return maxi;
+}
+
+int main() {
+
+  vector<vector<int> > matrix{{1,2,10,4},
+                              {100,3,2,1},
+                              {1,1,20,2},
+                              {1,2,2,1}};
+  
+  cout<<getMaxPathSum(matrix);
+}
+/*
+Output:
+
+105
+
+Time Complexity: O(N*N)
+
+Reason: At max, there will be M*N calls of recursion to solve a new problem,
+
+Space Complexity: O(N) + O(N*M)
+
+Reason: We are using a recursion stack space: O(N), where N is the path length and an external DP Array of size ‘N*M’.
