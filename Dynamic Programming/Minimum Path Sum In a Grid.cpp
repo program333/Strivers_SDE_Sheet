d68d77cf85b0e9d44267d5e39470ff7935eb3b8b
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-12 14:07:55 +0530

    Create Minimum Path Sum In a Grid.cpp

---
+++ Dynamic Programming/Minimum Path Sum In a Grid.cpp
@@ -0,0 +1,50 @@
+https://takeuforward.org/data-structure/minimum-path-sum-in-a-grid-dp-10/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minSumPathUtil(int i, int j,vector<vector<int>> &matrix,vector<vector<int>> &dp)
+{
+  if(i==0 && j == 0)
+    return matrix[0][0];
+  if(i<0 || j<0)
+    return 1e9;
+  if(dp[i][j]!=-1) return dp[i][j];
+    
+  int up = matrix[i][j]+minSumPathUtil(i-1,j,matrix,dp);
+  int left = matrix[i][j]+minSumPathUtil(i,j-1,matrix,dp);
+  
+  return dp[i][j] = min(up,left);
+  
+}
+
+int minSumPath(int n, int m, vector<vector<int> > &matrix){
+    vector<vector<int> > dp(n,vector<int>(m,-1));
+    return minSumPathUtil(n-1,m-1,matrix,dp);
+    
+}
+
+int main() {
+
+  vector<vector<int> > matrix{{5,9,6},
+                            {11,5,2}};
+                            
+  int n = matrix.size();
+  int m = matrix[0].size();
+  
+  cout<<minSumPath(n,m,matrix);
+}
+
+/*
+Output: 21
+
+Time Complexity: O(N*M)
+
+Reason: At max, there will be N*M calls of recursion.
+
+Space Complexity: O((M-1)+(N-1)) + O(N*M)
+
+Reason: We are using a recursion stack space:O((M-1)+(N-1)), here (M-1)+(N-1) is the path length and an external DP Array of size ‘N*M’.
+*/
