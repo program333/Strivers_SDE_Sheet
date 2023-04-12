65d75d96dc24980153092dccd595323b8b41f9ca
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-12 13:06:54 +0530

    Create nth Fibonacci number.cpp

---
+++ Dynamic Programming/nth Fibonacci number.cpp
@@ -0,0 +1,32 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n, vector<int>& dp){
+    if(n<=1) return n;
+    
+    if(dp[n]!= -1) return dp[n];
+    return dp[n]= f(n-1,dp) + f(n-2,dp);
+}
+
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  cout<<f(n,dp);
+  return 0;
+}
+
+/*
+Output: 5
+
+Time Complexity: O(N)
+
+Reason: The overlapping subproblems will return the answer in constant time O(1).
+Therefore the total number of new subproblems we solve is ‘n’. Hence total time complexity is O(N).
+
+Space Complexity: O(N)
+*/
