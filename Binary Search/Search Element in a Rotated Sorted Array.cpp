7d1fc130b3862eb7b3ca87ae266bc2b36bcd2f9e
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-13 17:37:22 +0530

    Update Search Element in a Rotated Sorted Array.cpp

--- Binary Search/Search Element in a Rotated Sorted Array.cpp
+++ Binary Search/Search Element in a Rotated Sorted Array.cpp
@@ -1,6 +1,18 @@
 https://takeuforward.org/data-structure/search-element-in-a-rotated-sorted-array/
 https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
 
+/*
+Approach :
+
+We divide the array into parts.
+It is done using two pointers, low and high, and dividing the range between them by 
+2. This gives the midpoint of the range. Check if the target is present in the midpoint, calculated before, of the array. 
+If not present, check if the left half of the array is sorted. 
+This implies that binary search can be applied in the left half of the array provided the target lies between the value range. 
+Else check into the right half of the array. Repeat the above steps until low <= high.
+If low > high indicates we have searched the array and the target is not present hence return -1. 
+Thus, it makes search operations less as we check the range first and then perform searching in possible ranges which may have target values.
+*/
 
 #include<bits/stdc++.h>
 
