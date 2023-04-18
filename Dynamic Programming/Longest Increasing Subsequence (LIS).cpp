5e6e90e00e81ba70d0b4d5bf38bd1726b8f69cd7
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-18 19:34:59 +0530

    Update Longest Increasing Subsequence (LIS).cpp

--- Dynamic Programming/Longest Increasing Subsequence (LIS).cpp
+++ Dynamic Programming/Longest Increasing Subsequence (LIS).cpp
@@ -1,5 +1,20 @@
 https://takeuforward.org/data-structure/longest-increasing-subsequence-dp-41/
 https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/
+https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+class Solution { 
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
 
 #include <bits/stdc++.h>
 using namespace std;
