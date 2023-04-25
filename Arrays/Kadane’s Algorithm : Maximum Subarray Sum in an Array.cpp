18270c201d0eb6c5ade5fac178805603af984696
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-04-26 01:35:01 +0530

    Update Kadane’s Algorithm : Maximum Subarray Sum in an Array.cpp

--- Arrays/Kadane’s Algorithm : Maximum Subarray Sum in an Array.cpp
+++ Arrays/Kadane’s Algorithm : Maximum Subarray Sum in an Array.cpp
@@ -48,45 +48,43 @@ Space Complexity: O(1)
 https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 
 // C++ program to print largest contiguous array sum
-
-#include <climits>
-#include <iostream>
+#include <iostream>  
 using namespace std;
-
-void maxSubArraySum(int a[], int size)
-{
-	int max_so_far = INT_MIN, max_ending_here = 0,
-		start = 0, end = 0, s = 0;
-
-	for (int i = 0; i < size; i++) {
-		max_ending_here += a[i];
-
-		if (max_so_far < max_ending_here) {
-			max_so_far = max_ending_here;
-			start = s;
-			end = i;
-		}
-
-		if (max_ending_here < 0) {
-			max_ending_here = 0;
-			s = i + 1;
-		}
-	}
-	cout << "Maximum contiguous sum is " << max_so_far
-		<< endl;
-	cout << "Starting index " << start << endl
-		<< "Ending index " << end << endl;
-}
-
-/*Driver program to test maxSubArraySum*/
-int main()
-{
-	int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-	int n = sizeof(a) / sizeof(a[0]);
-	maxSubArraySum(a, n);
-	return 0;
-}
-
+// Function that finds the largest sum contiguous array  
+int maxSubArraySum(int a[], int size)  
+{  
+    int current_sum = 0, maximum_sum =0,start = 0, end = 0, s = 0;;  
+   
+    for (int i = 0; i < size; i++)  
+    {  
+        current_sum = current_sum + a[i];  
+        if (current_sum > maximum_sum){  
+           maximum_sum = current_sum; 
+           start = s;
+           end = i;
+        }
+   
+        if (current_sum < 0)  {
+           current_sum = 0;
+            s = i + 1;
+        }
+    }
+    cout << "Maximum contiguous sum is " << maximum_sum
+         << endl;
+    cout << "Starting index " << start << endl
+         << "Ending index " << end << endl;
+         
+    return maximum_sum;  
+}  
+// main method  
+int main()  
+{  
+    int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+    int n = sizeof(a)/sizeof(a[0]);  
+    maxSubArraySum(a, n);  
+ 
+    return 0;  
+}  
 /*
 Time Complexity: O(N), where N = size of the array.
 Reason: We are using a single loop running N times.
