6994c6583f35a115d9950462c7435a03255d6f03
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-06-25 03:02:27 +0530

    Update Search in a 2 D matrix.cpp

--- Binary Search/Search in a 2 D matrix.cpp
+++ Binary Search/Search in a 2 D matrix.cpp
@@ -1,61 +1,54 @@
-https://www.geeksforgeeks.org/search-in-row-wise-and-column-wise-sorted-matrix/
 
+https://www.geeksforgeeks.org/search-in-a-row-wise-and-column-wise-sorted-2d-array-using-divide-and-conquer-algorithm/
 
-// C++ program to search an element in row-wise
-// and column-wise sorted matrix
 #include <bits/stdc++.h>
+#define ROW 4
+#define COL 4
 using namespace std;
 
-/* Searches the element x in mat[][]. If the
-element is found, then prints its position
-and returns true, otherwise prints "not found"
-and returns false */
-int search(int mat[4][4], int n, int x)
+bool binary_search(int mat[ROW][COL], int row, int start,
+				int end, int key)
 {
-	if (n == 0)
-		return -1;
-
-	int smallest = mat[0][0], largest = mat[n - 1][n - 1];
-	if (x < smallest || x > largest)
-		return -1;
-
-	// set indexes for top right element
-	int i = 0, j = n - 1;
-	while (i < n && j >= 0) {
-		if (mat[i][j] == x) {
-			cout << "Element found at " << i << ", " << j;
-			return 1;
+	while (start <= end) {
+		int mid = start + (end - start) / 2;
+		if (mat[row][mid] == key) {
+			return true;
 		}
-		if (mat[i][j] > x)
-			j--;
-
-		// Check if mat[i][j] < x
-		else
-			i++;
+		else if (mat[row][mid] < key) {
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
 	}
+	return false;
+}
 
-	cout << "n Element not found";
-	return 0;
+void search(int mat[ROW][COL], int key)
+{
+	// search in each row
+	for (int i = 0; i < ROW; i++) {
+		// perform binary search in current row
+		if (mat[i][0] <= key && mat[i][COL - 1] >= key) {
+			if (binary_search(mat, i, 0, COL - 1, key)) {
+				cout << "Found " << key << " at " << i
+					<< " " << endl;
+				return;
+			}
+		}
+	}
+	cout << key << " not found" << endl;
 }
 
 // Driver code
 int main()
 {
-	int mat[4][4] = { { 10, 20, 30, 40 },
-					{ 15, 25, 35, 45 },
-					{ 27, 29, 37, 48 },
-					{ 32, 33, 39, 50 } };
-
-	// Function call
-	search(mat, 4, 29);
-
+	int mat[ROW][COL] = { { 10, 20, 30, 40 },
+						{ 15, 25, 35, 45 },
+						{ 27, 29, 37, 48 },
+						{ 32, 33, 39, 50 } };
+	int key = 50;
+	search(mat, key);
 	return 0;
 }
 
-/*
-Output
-Element found at 2, 1
-Time Complexity: O(N), Only one traversal is needed, i.e, i from 0 to n and j from n-1 to 0 with at most 2*N steps. 
-The above approach will also work for the M x N matrix (not only for N x N). Complexity would be O(M + N)
-Auxiliary Space: O(1), No extra space is required
-*/
