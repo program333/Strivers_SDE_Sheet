2143491c047f7b4db613126b595f88597637febd
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-06-30 00:07:54 +0530

    Update Longest Subarray with given Sum K (Only Positive Numbers).cpp

--- Arrays/Longest Subarray with given Sum K (Only Positive Numbers).cpp
+++ Arrays/Longest Subarray with given Sum K (Only Positive Numbers).cpp
@@ -53,31 +53,39 @@ Space Complexity: O(1), we are not using any extra space
 
 
 
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int getLongestSubarray(vector<int>& a, long long k) {
     int n = a.size(); // size of the array.
 
-    int left = 0, right = 0; // 2 pointers
-    long long sum = a[0];
+    map<long long, int> preSumMap;
+    long long sum = 0;
     int maxLen = 0;
-    while (right < n) {
-        // if sum > k, reduce the subarray from left
-        // until sum becomes less or equal to k:
-        while (left <= right && sum > k) {
-            sum -= a[left];
-            left++;
-        }
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
 
-        // if sum = k, update the maxLen i.e. answer:
+        // if the sum = k, update the maxLen:
         if (sum == k) {
-            maxLen = max(maxLen, right - left + 1);
+            maxLen = max(maxLen, i + 1);
         }
 
-        // Move forward thw right pointer:
-        right++;
-        if (right < n) sum += a[right];
+        // calculate the sum of remaining part i.e. x-k:
+        long long rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
     }
 
     return maxLen;
@@ -91,3 +99,5 @@ int main()
     cout << "The length of the longest subarray is: " << len << "\n";
     return 0;
 }
+
+
