77b30a215ab986585252e175d01bd858218b512c
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-08-19 15:53:06 +0530

    Update Remove Outermost Parentheses.cpp

--- String/Remove Outermost Parentheses.cpp
+++ String/Remove Outermost Parentheses.cpp
@@ -22,3 +22,12 @@ public:
         return res;
     }
 };
+
+/*
+Complexity
+Time complexity:
+The time complexity of this solution is O(n), where n is the length of the input string. This is because each character in the string is processed once and the push and pop operations on the stack take O(1) time each.
+
+Space complexity:
+The space complexity of this solution is O(n), where n is the length of the input string. This is because the maximum size of the stack is n/2 (if all the parentheses are opening parentheses), and in the worst case, the result string can also have a size of n/2.
+*/
