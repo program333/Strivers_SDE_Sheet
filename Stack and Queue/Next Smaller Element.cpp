a18127fd34674d134877b2bf74cad83af77ba69a
Devindra Hirabatti <94212530+program333@users.noreply.github.com>
2023-09-15 23:27:53 +0530

    Update Next Smaller Element.cpp

--- Stack and Queue/Next Smaller Element.cpp
+++ Stack and Queue/Next Smaller Element.cpp
@@ -1,7 +1,27 @@
 
 https://www.geeksforgeeks.org/next-smaller-element/
 
-
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<int> help_classmate(vector<int> arr, int n){
+        // Your code here
+        stack<int> s;
+        vector<int> ans;
+        for(int i=n-1; i>=0; i--){
+            while(!s.empty()&&s.top()>=arr[i])
+                s.pop();
+            if(s.empty())
+                ans.push_back(-1);
+            else
+                ans.push_back(s.top());
+            s.push(arr[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
 
 // Simple C++ program to print
 // next smaller elements in a given array
